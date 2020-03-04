dc174b0228d3f86a83a4265ad26a1bde3edc97d6
ivan <ivanruch@gmail.com>
2020-03-04 13:52:06 +0100

    Display the BigChain DB URL

--- frontend/src/services/serviceContext.ts
+++ frontend/src/services/serviceContext.ts
@@ -2,11 +2,13 @@ import {config} from '../config';
 import {ApiService} from './apiService';
 import {createContext} from 'react';
 import {VerificationService} from './verificationService';
+import {BigchainService} from './bigchainService';
 
 export const setup = () => {
   const apiService = new ApiService(config.apiUrl);
   return {
-    verificationService: new VerificationService(apiService)
+    verificationService: new VerificationService(apiService),
+    bigchainService: new BigchainService(config.bigchaindbUrl)
   };
 };
 
--- frontend/src/ui/Dashboard/Footer.tsx
+++ frontend/src/ui/Dashboard/Footer.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Logo1x from '../../assets/steelButSmart@1x.png';
 import Logo2x from '../../assets/steelButSmart@2x.png';
+import {useServices} from '../hooks/useServices';
 
-export const Footer = () => (
-  <footer>
-    <div className='logo'>
-      <p className='logo-text'>Powered by</p>
-      <img className='logo-image' src={Logo1x} srcSet={Logo2x} alt='logo'/>
-    </div>
-  </footer>
-);
+export const Footer = () => {
+  const {bigchainService} = useServices();
+
+  return (
+    <footer>
+      <div className='logo'>
+        <p className='logo-text'>Powered by</p>
+        <img className='logo-image' src={Logo1x} srcSet={Logo2x} alt='logo'/>
+      </div>
+      <div>
+        <p className='connected-text'>
+          Connected to <a href={bigchainService.bigchainUrl.href} className='bcdb-link'>{bigchainService.hostname}</a>
+        </p>
+      </div>
+    </footer>
+  );
+};
